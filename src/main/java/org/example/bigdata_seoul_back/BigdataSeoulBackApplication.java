f302902f0bb20a7e407c88af5fd12ced3b389cd9
CHO, NAEUN <jo044879@gmail.com>
2026-04-26 19:48:38 +0900

    init back

---
+++ src/main/java/org/example/bigdata_seoul_back/BigdataSeoulBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.bigdata_seoul_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataSeoulBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataSeoulBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bigdata_seoul_back/BigdataSeoulBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bigdata_seoul_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataSeoulBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
